f85488c60b920291ec23816d9a6f0f6d87b5e1a1
ashok10 <ashok969696@gmail.com>
2025-10-18 19:45:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,45 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.8.1"
+
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-south-1a", "ap-south-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Name = "eks-vpc"
+  }
+}
+
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.21"
+
   cluster_name    = var.cluster_name
   cluster_version = "1.29"
-  subnet_ids      = ["subnet-xxxx", "subnet-yyyy"] # Replace with your actual subnet IDs
-  vpc_id          = "vpc-xxxx"                     # Replace with your VPC ID
 
-  manage_aws_auth_configmap = true
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  cluster_endpoint_public_access = true
 
   eks_managed_node_groups = {
     default = {
-      desired_size = 2
-      max_size     = 3
-      min_size     = 1
-      instance_types = ["t3.medium"]
+      instance_types = ["t3.micro"]
+      min_size       = 1
+      max_size       = 2
+      desired_size   = 1
     }
+  }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
   }
 }
