5e71580b7383ced6a6a08858b425cd5257c90e3a
ashok10 <ashok969696@gmail.com>
2025-10-18 19:57:18 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
-terraform {
+5b2terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket-eks"
+    bucket         = "my-terraform-state-bucket-eks5b2"
     key            = "eks/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-lock"
